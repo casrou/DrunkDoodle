12372f1a60f2cb9c7832b75476b0628e2f406315
casrou (xps) <casper.nc97@gmail.com>
2018-10-17 03:39:10 +0200

    Fixed public Release issues, added fullscreen functionality

--- DrunkDoodle.Server/Startup.cs
+++ DrunkDoodle.Server/Startup.cs
@@ -20,12 +20,12 @@ namespace DrunkDoodle.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -47,9 +47,9 @@ namespace DrunkDoodle.Server
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseCookiePolicy();
             app.UseSignalR(routes =>
             {
                 routes.MapHub<GameHub>("/gameHub");
