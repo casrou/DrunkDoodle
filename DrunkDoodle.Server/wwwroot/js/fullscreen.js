867acc1c47c404b99ecb27eb5459d8181b5f9e05
casrou (xps) <casper.nc97@gmail.com>
2018-10-17 13:19:40 +0200

    refactored fullscreen javascript

---
+++ DrunkDoodle.Server/wwwroot/js/fullscreen.js
@@ -0,0 +1,55 @@
+﻿"use strict";
+
+if (!isiPhone()) {
+    $("#fullscreen").show();
+}
+
+var fullscreen = false;
+
+$("#fullscreen").click(function () {
+    if (!isiPhone()) {
+        if (fullscreen) {
+            closeFullscreen();
+            fullscreen = false;
+        } else {
+            openFullscreen();
+            fullscreen = true;
+        }
+    }
+});
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
+
+function isiPhone() {
+    return (
+        (navigator.platform.indexOf("iPhone") !== -1) ||
+        (navigator.platform.indexOf("iPod") !== -1)
+    );
+}
